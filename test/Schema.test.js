36768d4f521efe3e946b533ecfa7275aacd3e0ac
Yarden Sod-Moriah <yardnsm@gmail.com>
2016-03-24 11:45:21 +0200

    added schema validator

--- test/Schema.test.js
+++ test/Schema.test.js
@@ -1,14 +1,63 @@
-var Schema = require("../lib/Schema");
-var expect = require("chai").expect;
+var Schema = require('../lib/Schema');
+var expect = require('chai').expect;
 
-describe.only("Schema", function () {
+describe.only('Schema', function () {
 
-    describe("constructor", function () {
+    describe('constructor', function () {
 
-        it("should create an instance", function (done) {
+        it('should create an instance', function (done) {
             var s = new Schema({});
             expect(s).to.be.instanceOf(Schema);
             done();
         });
+    });
+
+    describe('validator', function () {
+
+        it('should return true if Schema is fine', function (done) {
+            var s = new Schema({
+                prop1: String,                                     // Only a type
+                prop2: { type: Number, required: false },          // With a 'definition'
+                prop3: { type: { type: Boolean }, foo: String },   // Type is actually an inner-child (virtual)
+                prop4: { foo: { bar: { baz: Date} } },             // Inner child
+                prop5: [{                                          // An array (child schema)
+                    prop1: String,
+                    prop2: { type: Number, required: false },
+                    prop3: { type: { type: Boolean }, foo: String },
+                    prop4: { foo: { bar: { baz: Date} } }
+                }]
+            });
+
+            expect(s.validate()).to.be.true;
+            done();
+        });
+
+        it('should throw a property is null', function (done) {
+            var s = new Schema({
+                prop1: null
+            });
+
+            expect(s.validate.bind(s)).to.throw(Error);
+            done();
+        });
+
+        it('should throw an error if there is a wrong type', function (done) {
+            var s = new Schema({
+                prop1: 'not-a-type'
+            });
+
+            expect(s.validate.bind(s)).to.throw(Error);
+            done();
+        });
+
+        it('should throw an error if there is a wrong definition', function (done) {
+            var s = new Schema({
+                prop1: { type: String, shouldNoBeHere: 'wat' }
+            });
+
+            expect(s.validate.bind(s)).to.throw(Error);
+            done();
+        });
+
     });
 });
